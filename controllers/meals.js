b67ca0ab2baf203e64b78e6b5ab3d8cc1e32380e
Illia Brytun <elbrytun@gmail.com>
2022-03-02 00:14:12 -0500

    implement functionality to add new meal

--- controllers/meals.js
+++ controllers/meals.js
@@ -2,10 +2,21 @@ import { Meal } from '../models/meal.js'
 
 
 function newMeal(req, res) {
-  res.render('meals/new', {title: 'Add meal'})
+  Meal.find({}, function(err, meals) {
+  res.render('meals/new', {
+    title: 'Meals',
+    meals
+    })
+  })
 }
 
+function create(req, res) {
+  Meal.create(req.body, function(error, meal) {
+    res.redirect('/meals/new')
+  })
+}
 
 export {
-  newMeal as new
+  newMeal as new,
+  create
 }
